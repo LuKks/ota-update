16e3ceb9f64a6031734ca5008049b05df588cdb8
Lucas Barrena <lucasbarrena98@gmail.com>
2024-07-12 14:59:26 -0300

    Add `--save` option to create cmd

--- bin/create.js
+++ bin/create.js
@@ -2,6 +2,7 @@ const { createCommand } = require('commander')
 
 module.exports = createCommand('create')
   .description('Create a new firmware')
+  .option('--save', 'save .env file with credentials')
   .option('--server [url]', 'custom backend')
   .option('--secret [token]', 'access token')
   .action(require('../lib/create.js'))
